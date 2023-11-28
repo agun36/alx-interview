505e466611bcedd6aded1f6ac496f810651ecf1a
agun36 <aguntoroti10@gmail.com>
2023-11-28 13:16:57 +0000

    Fix bug in login functionality

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,12 +5,7 @@
 
 def pascal_triangle(n):
     """
-    Generates a Pascal's triangle of size n.
-    Parameters:
-        n (int): The size of the Pascal's triangle.
-    Returns:
-        list of lists: A list of lists representing the
-        Pascal's triangle.
+    Pascal's triangle
     """
     if n <= 0:
         return []
@@ -18,7 +13,7 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)
         triangle.append(row)
     return triangle
