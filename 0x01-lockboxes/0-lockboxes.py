a9c54e8b6fd82cc1c392d3e74ba526d04598c914
agun36 <aguntoroti10@gmail.com>
2023-12-05 14:41:21 +0000

    0-lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,15 +6,16 @@
 
 def canUnlockAll(boxes):
     """Determines if boxes can be unlocked"""
-    visited = set()
-    queue = [0]
+    position = 0
+    unlocked = {}
 
-    while queue:
-        current_box = queue.pop(0)
-        visited.add(current_box)
-
-        for key in boxes[current_box]:
-            if key not in visited:
-                queue.append(key)
-
-    return len(visited) == len(boxes)
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
+    return False
